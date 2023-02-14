a26f489b9446b0337bc50126edcef2a632c6f540
jordan-ort <122242791+jordan-ort@users.noreply.github.com>
2023-02-14 13:20:56 -0500

    scripts for determining amino acid residues enriched by host

---
+++ end-aa-analysis/analyze-aa-counts.py
@@ -0,0 +1,85 @@
+import pandas as pd
+from scipy.stats import fisher_exact
+
+## specify directory, files, and gene
+tsv_dir = '/Users/jort/Desktop/test/' # directory containing tsv files from get-all-aa-counts.py
+host1_file = 'human_all_aa_counts.tsv' # host1 (enriched in host) tsv file
+host2_file = 'avian_all_aa_counts.tsv' # host2 (background host) tsv file
+output_file = 'all_aa_or_pv.tsv' # output for dataframe with odds ratios and pvalues (.tsv)
+gene = 'PB2' # gene to analyze
+
+
+##### user input above #####
+
+
+def cal_enr(host1_df_col, host2_df_col):
+    '''calculate the odds ratio and pvalue for each amino acid at a given position'''
+
+    ## get list of AAs with nonzero counts in each host
+    host1_df_col_nonzero_aas = host1_df_col.loc[(host1_df_col != 0)].index.values.tolist()
+    host2_df_col_nonzero_aas = host2_df_col.loc[(host2_df_col != 0)].index.values.tolist()
+
+    ## get list of unique nonzero AAs, excluding 'other'
+    nonzero_aas = [x for x in set(host1_df_col_nonzero_aas + host2_df_col_nonzero_aas) if x != 'other']
+
+    pos_scores = {}
+
+    ## for each nonzero AA, calculate the odds ratio and pvalue using a Fisher's exact test and assign to dict
+    for aa in nonzero_aas:
+        presence_host1 = host1_df_col.loc[aa]
+        absence_host1 = sum(host1_df_col.loc[~host1_df_col.index.isin([aa])])
+        presence_host2 = host2_df_col.loc[aa]
+        absence_host2 = sum(host2_df_col.loc[~host2_df_col.index.isin([aa])])
+        oddsr, p = fisher_exact([[presence_host1, absence_host1],[presence_host2, absence_host2]], alternative='two-sided')
+        pos_scores[aa] = (oddsr, p)
+
+    return pos_scores
+
+
+## load tsv files containing AA counts
+host1_df_path = tsv_dir + host1_file
+host2_df_path = tsv_dir + host2_file
+host1_df = pd.read_csv(host1_df_path, sep='\t', header=0, index_col=0)
+host2_df = pd.read_csv(host2_df_path, sep='\t', header=0, index_col=0)
+
+
+## get list of all positions based on gene length and make list of all AAs
+if gene == 'HA':
+    aalength = 568 # 568 AAs in HA
+elif gene == 'PB2':
+    aalength = 759 # 759 AAs in PB2
+all_positions = range(1, aalength+1)
+all_aas = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
+
+
+## create dictionary to store data from each position
+all_pos_scores = {}
+
+## get single columns from dataframes corresponding to each position,
+## then get a list of odds ratios and pvalues for each AA at that position
+## and add this list to dictionary with the position as its key
+for pos in all_positions:
+    host1_col = host1_df.loc[:,str(pos)]
+    host2_col = host2_df.loc[:,str(pos)]
+    all_pos_scores[pos] = cal_enr(host1_col, host2_col)
+
+
+## create lists for position, AA, odds ratio, and pvalue
+pos_list = []
+aa_list = []
+or_list = []
+pv_list = []
+
+## append data to lists for each position/AA pair
+for pos in all_pos_scores:
+  for aa in all_pos_scores[pos].keys():
+    pos_list.append(pos)
+    aa_list.append(aa)
+    or_list.append(all_pos_scores[pos][aa][0])
+    pv_list.append(all_pos_scores[pos][aa][1])
+
+
+## generate dataframe from data and save as a tsv file
+output_df = pd.DataFrame({'position': pos_list, 'aminoacid': aa_list, 'oddsratio': or_list, 'pvalue': pv_list})
+output_path = tsv_dir + output_file
+output_df.to_csv(output_path, sep='\t', header=True, index=False)
---
+++ end-aa-analysis/get-all-aa-counts.py
@@ -0,0 +1,74 @@
+import json
+import imp
+import pandas as pd
+
+## specify directory, files, hosts, and gene
+baltic_path = '/Users/jort/coding/baltic/baltic/baltic.py' # path to baltic.py file (https://github.com/evogytis/baltic)
+json_dir = '/Users/jort/Desktop/test/' # directory containing json file; output files to be saved here
+tree_file = 'flu_avian_h5n1_pb2_mutprop.json' # json tree with mutations propagated
+host1 = 'Human' # host to screen for enrichment in
+host2 = 'Avian' # background host
+host1_output_file = 'human_all_aa_counts.tsv' # output for host1 dataframe (.tsv)
+host2_output_file = 'avian_all_aa_counts.tsv' # output for host2 dataframe (.tsv)
+gene = 'PB2' # gene to analyze
+
+
+##### user input above #####
+
+
+## import baltic
+bt = imp.load_source('baltic', baltic_path)
+
+def read_in_tree_json(input_tree):
+    '''read in a tree in json format'''
+    with open(input_tree) as json_file:
+        json_tree = json.load(json_file)
+    json_translation = {'absoluteTime':lambda k: k.traits['node_attrs']['num_date']['value'],'name':'name'} ## allows baltic to find correct attributes in JSON, height and name are required at a minimum
+    bt_tree, meta = bt.loadJSON(json_tree, json_translation)
+    return json_tree, bt_tree
+
+## read in json tree
+json_file = json_dir + tree_file
+json_tree, tree = read_in_tree_json(json_file)
+
+## get full AA sequence for each leaf
+all_seqs = []
+for k in tree.Objects:
+    host = k.traits['node_attrs']['host']['value']
+    seq_dict = {}
+    if k.branchType == 'leaf':
+        for mut in k.traits['branch_attrs']['mutations'][gene]:
+           seq_dict[int(mut[1:len(mut)-1])] = mut[-1]
+    all_seqs.append([host, seq_dict])
+
+
+## generate host count dicts for each amino acid at each position
+if gene == 'HA':
+    aalength = 568 # 568 AAs in HA
+elif gene == 'PB2':
+    aalength = 759 # 759 AAs in PB2
+all_positions = range(1, aalength+1)
+all_aas = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y', 'other']
+host1_dict = dict((pos, dict((aa, 0) for aa in all_aas)) for pos in all_positions)
+host2_dict = dict((pos, dict((aa, 0) for aa in all_aas)) for pos in all_positions)
+
+## iterate through each sequence and record counts of AA occurrences in each host
+for [host, seq_dict] in all_seqs:
+  if host == host1:
+    for k, v in seq_dict.items():
+      if v in all_aas and k in list(host1_dict):
+        host1_dict[k][v] += 1
+      elif v not in all_aas and k in list(host1_dict):
+        host1_dict[k]['other'] += 1
+  elif host == host2:
+    for k, v in seq_dict.items():
+      if v in all_aas and k in list(host2_dict):
+        host2_dict[k][v] += 1
+      elif v not in all_aas and k in list(host2_dict):
+        host2_dict[k]['other'] += 1
+
+## save host count dataframes as tsv files
+host1_output_path = json_dir + host1_output_file
+host2_output_path = json_dir + host2_output_file
+pd.DataFrame.from_dict(host1_dict).to_csv(host1_output_path, sep='\t', header=True, index=True)
+pd.DataFrame.from_dict(host2_dict).to_csv(host2_output_path, sep='\t', header=True, index=True)
